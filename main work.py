ac3a27bcaa56f862b6ef466f8f1762f537d72603
shukur-alom <shukuralom1234@gmail.com>
2021-06-09 22:54:38 +0600

    code edit... litewat

--- main work.py
+++ main work.py
@@ -1,16 +1,15 @@
-import cv2,timeit
+import cv2
 from pickle import load
 from datetime import datetime
 from numpy import argmin
-import face_recognition
+from face_recognition import face_locations,face_encodings,face_distance
 
 
 encodin_list = load( open( "encodin list.p", "rb" ) )
 class_name = load( open( "class name.p", "rb" ) )
 
-
 def attendence(name):
-    with open('att.csv','r+') as f:
+    with open('Today attendance.csv','r+') as f:
         myData = f.readlines()
         namelist = []
         for line in myData:
@@ -20,27 +19,25 @@ def attendence(name):
             f.writelines(f"\n{name},{datetime.now()}")
 
 def detact_human(img_s):
-    img_s = cv2.resize(img, (0,0), None, 0.25, 0.25)
+    img_s = cv2.resize(img, (0,0), None, 0.33, 0.33)
 
     img_s = cv2.cvtColor(img_s, cv2.COLOR_BGR2RGB)
-
     faceLoc_,name_ = [],[]
 
-    facesCurFrame = face_recognition.face_locations(img_s)
-    encodeCurFrame = face_recognition.face_encodings(img_s, facesCurFrame)
+    facesCurFrame = face_locations(img_s)
+    encodeCurFrame = face_encodings(img_s, facesCurFrame)
 
-    for i in range(3):
+    for i in range(5):
         for encodeFace,faceLoc in zip(encodeCurFrame,facesCurFrame):
 
-            faceDis = face_recognition.face_distance(encodin_list , encodeFace)
+            faceDis = face_distance(encodin_list , encodeFace)
 
             find = argmin(faceDis)
             name = class_name[find].upper()
 
-            if str(name in name_) == 'False' and faceDis[find]*100 < 52.00:
+            if str(name in name_) == 'False' and faceDis[find]*100 < 48.00:
                 faceLoc_.append(faceLoc)
                 name_.append(name)
-
     return faceLoc_,name_
 
 cap = cv2.VideoCapture(0)
@@ -55,16 +52,15 @@ while 1:
 
         y1,x2,y2,x1 = faceLoc
 
-        y1,x2,y2,x1 = y1*4, x2*4, y2*4, x1*4
+        y1,x2,y2,x1 = y1*3, x2*3, y2*3, x1*3
 
-        cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0),2 )
+        img = cv2.rectangle(img, (x1,y1), (x2,y2), (36,255,12), 1)
 
-        cv2.putText(img, name, (x1+6 ,y2-6), cv2.FONT_HERSHEY_SIMPLEX, 0.50, (0,255,255), 2)
+        cv2.putText(img, name, (x1, y1-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 166, 255), 1)
+
         attendence(name)
 
     cv2.imshow("Cam_1", img)
     if cv2.waitKey(1) == ord('q'): break
 
 cv2.destroyAllWindows()()
-    
-    
